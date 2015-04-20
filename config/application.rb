31af78371fa90927f46357fdf46c728265fd313c
Matt Shwery <mshwery@gmail.com>
2015-04-19 19:40:36 -0500

    add spec class

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Icd10
         g.javascripts = false
         g.helper = false
     end
+    
+    config.autoload_paths += %W(\#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
